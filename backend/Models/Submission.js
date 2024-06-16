e311df7aedf97ac4b912f59845039d768bef2abb
Bibek <ba03021321@ku.edu.np>
2024-06-16 13:02:57 +0545

    backend

--- backend/Models/Submission.js
+++ backend/Models/Submission.js
@@ -1,30 +1,28 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const SubmissionSchema = new Schema({
-    submissionDate:{
-        type:Date,
-        default:Date.now
-    },
-    assignmentId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Assignment',
-        required: true
-    },
-    studentId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required: true
-    },
-    content:{
-        type:String,
-        required:true,
-    },
-    status:{
-        type:String,
-        enum:["Submitted","Pending","Late submission"],
-        default:"Pending"
-    }
-})
+  submissionDate: {
+    type: Date,
+    default: Date.now,
+  },
+  assignmentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Assignment",
+  },
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Submitted", "Pending", "Late submission"],
+    default: "Pending",
+  },
+});
 const submissionModel = mongoose.model("Submission", SubmissionSchema);
 module.exports = submissionModel;
--- backend/Routes/SubmissionRoutes.js
+++ backend/Routes/SubmissionRoutes.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 
-const {submitAssignment, getSubmittedAssignments,getPendingAssignments,
-    countSubmittedAssignments,countLateSubmissions} = require('../Controllers/submissionController')
+const {
+  submitAssignment,
+  getSubmittedAssignments,
+  getPendingAssignments,
+  countSubmittedAssignments,
+  countLateSubmissions,
+} = require("../Controllers/submissionController");
 
-router.post('/createSubission',submitAssignment)
-router.get('/getSubmittedAssignments',getSubmittedAssignments)
-router.get('/getPendingAssignments',getPendingAssignments)
-router.get('/countSubmittedAssignments',countSubmittedAssignments)
-router.get('/countLateSubmissions',countLateSubmissions)
+router.post("/createSubmission", submitAssignment);
+router.get("/getSubmittedAssignments", getSubmittedAssignments);
+router.get("/getPendingAssignments", getPendingAssignments);
+router.get("/countSubmittedAssignments", countSubmittedAssignments);
+router.get("/countLateSubmissions", countLateSubmissions);
 
-module.exports=router
+module.exports = router;
