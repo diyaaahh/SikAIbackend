51ff41e4e9d7948c0be7a94436b34e54bd5e9775
Bibek <ba03021321@ku.edu.np>
2024-06-17 21:54:03 +0545

    added teacher

--- backend/Routes/StudentTrackerRoutes.js
+++ backend/Routes/StudentTrackerRoutes.js
@@ -1,7 +1,13 @@
+const Router = require("express").Router();
+const {
+  addStudentTracker,
+  getStudentTracker,
+  updateStudentTracker,
+  getAllStudentTrackers,
+} = require("../Controllers/StudentTrackerController");
+Router.post("/add", addStudentTracker);
+Router.get("/:studentId", getStudentTracker);
+Router.put("/:studentId", updateStudentTracker);
+Router.get("/getAll", getAllStudentTrackers);
 
-const Router  = require('express').Router();
-const {addStudentTracker, getStudentTracker, updateStudentTracker} = require('../Controllers/StudentTrackerController');
-Router.post('/add', addStudentTracker);
-Router.get('/:studentId', getStudentTracker);
-Router.put('/:studentId', updateStudentTracker);
 module.exports = Router;
